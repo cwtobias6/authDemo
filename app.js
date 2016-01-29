06e87c3c2d979fb860aed4dd2fd3c42332aa9117
Christian Tobias <cwtobias6@gmail.com>
2016-01-29 10:13:39 -0600

    added user model

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var express = require("express");
-var mongoose.connect("mongodb://localhost/authdemo");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
+
+mongoose.connect("mongodb://localhost/authdemo");
 
 var app = express();
+
 app.set("view engine" , "ejs");
+app.use(require("express-session")({
+	secret: "A Millie A Millie",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get("/", function(req,res) {
 	res.render("home");
